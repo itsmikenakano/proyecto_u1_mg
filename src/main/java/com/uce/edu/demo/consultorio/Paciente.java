dab2e301d97a8ea2b2f41b3efcc27c5b040db4e8
Michael Garcia <msgr.2300290992@gmail.com>
2022-05-27 20:05:35 -0500

    Mi primera subida desde consola

---
+++ src/main/java/com/uce/edu/demo/consultorio/Paciente.java
@@ -0,0 +1,24 @@
+package com.uce.edu.demo.consultorio;
+
+public class Paciente {
+	private String nombre;
+	private Integer edad;
+
+	// SET Y GET
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+
+}
