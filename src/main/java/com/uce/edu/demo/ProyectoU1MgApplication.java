a44ca812748a9caf59247491d38b0e3825c61dd2
Michael Garcia <msgr.2300290992@gmail.com>
2022-06-07 20:14:35 -0500

    Taller 8: Ejercicio 2 de componentes de negocio

--- src/main/java/com/uce/edu/demo/ProyectoU1MgApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU1MgApplication.java
@@ -1,31 +1,22 @@
 package com.uce.edu.demo;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.modelo.Estudiante;
-import com.uce.edu.demo.modelo.Materia;
-import com.uce.edu.demo.modelo.Matricula;
-import com.uce.edu.demo.service.IEstudianteService;
-import com.uce.edu.demo.service.IMateriaService;
-import com.uce.edu.demo.service.IMatriculaService;
+import com.uce.edu.demo.banco.service.IDepositoService;
+import com.uce.edu.demo.banco.service.ITransferenciaService;
 
 @SpringBootApplication
 public class ProyectoU1MgApplication implements CommandLineRunner {
 
 	@Autowired
-	private IEstudianteService estudianteService;
-
+	private ITransferenciaService iTransferenciaService;
 	@Autowired
-	private IMateriaService materiaService;
-
-	@Autowired
-	private IMatriculaService matriculaService;
+	private IDepositoService depositoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1MgApplication.class, args);
@@ -34,72 +25,9 @@ public class ProyectoU1MgApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Estudiante e = new Estudiante();
-		e.setNombre("Edison");
-		e.setApellido("Cayambe");
-		e.setCedula("1223344567");
-		this.estudianteService.ingresarEstudiante(e);
+		this.iTransferenciaService.realizarTransferencia("12", "13", new BigDecimal(20));
 
-		Estudiante e1 = new Estudiante();
-		e1.setNombre("Pepito");
-		e1.setApellido("Perez");
-		e1.setCedula("0932838323");
-		this.estudianteService.ingresarEstudiante(e1);
-
-		// Usar los 3 metodos restantes
-		e.setCedula("1732535353");
-		this.estudianteService.actualizarEstudiante(e);
-
-		this.estudianteService.buscarPorApellido("Cayambe");
-
-		this.estudianteService.eliminarEstudiante("0932838323");
-
-		System.out.println();
-
-		// Materia
-		Materia m = new Materia();
-		m.setNombre("Base de datos I");
-		m.setSemestre("5");
-		this.materiaService.insertarMateria(m);
-
-		Materia m1 = new Materia();
-		m1.setNombre("Inferencia Estadistica");
-		m1.setSemestre("5");
-		this.materiaService.insertarMateria(m1);
-
-		m.setNombre("Base de datos II");
-		this.materiaService.actualizarMateria(m);
-
-		this.materiaService.buscarMateria("Inferencia Estadistica");
-
-		this.materiaService.eliminarMateria("Base de datos II");
-
-		System.out.println();
-
-		// Matricula
-		List<Materia> listaMateria = new ArrayList<Materia>();
-		listaMateria.add(m);
-		listaMateria.add(m1);
-
-		Matricula matricula = new Matricula();
-		matricula.setEstudiante(e);
-		matricula.setNumero("1234");
-		matricula.setMateria(listaMateria);
-		this.matriculaService.insertarMatricula(matricula);
-
-		Matricula matricula1 = new Matricula();
-		matricula1.setEstudiante(e);
-		matricula1.setNumero("12345");
-		matricula1.setMateria(listaMateria);
-		this.matriculaService.insertarMatricula(matricula1);
-
-		matricula.setNumero("123456");
-		this.matriculaService.actualizarMatricula(matricula);
-
-		this.matriculaService.buscarMatricula("123456");
-
-		this.matriculaService.eliminarMatricula("12345");
-
+		this.depositoService.realizarDeposito("14", new BigDecimal(50));
 	}
 
 }
---
+++ src/main/java/com/uce/edu/demo/banco/repository/IDepositoRepository.java
@@ -0,0 +1,8 @@
+package com.uce.edu.demo.banco.repository;
+
+import com.uce.edu.demo.banco.modelo.Deposito;
+
+public interface IDepositoRepository {
+	
+	public void insertarDeposito(Deposito d);
+}
