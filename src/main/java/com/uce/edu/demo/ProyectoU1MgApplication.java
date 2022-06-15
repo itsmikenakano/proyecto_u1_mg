ba026f89eb69fc855b68e7576a8d98fc821c2595
Michael Garcia <msgr.2300290992@gmail.com>
2022-06-14 20:03:09 -0500

    Taller 11: Ámbitos/Scope de Beans en Spring Framework

--- src/main/java/com/uce/edu/demo/ProyectoU1MgApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU1MgApplication.java
@@ -1,26 +1,36 @@
 package com.uce.edu.demo;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.banco.service.IFachadaCuentaBancaria;
-import com.uce.edu.demo.bodega.modelo.Producto;
-import com.uce.edu.demo.bodega.service.IGestorInventario;
-import com.uce.edu.demo.bodega.service.IProductoService;
+import com.uce.edu.demo.modelo.Estudiante;
+import com.uce.edu.demo.modelo.Materia;
+import com.uce.edu.demo.modelo.Matricula;
+import com.uce.edu.demo.modelo.ProfesorGeneral;
+import com.uce.edu.demo.modelo.ProfesorMateria;
+import com.uce.edu.demo.service.IMatriculaService;
 
 @SpringBootApplication
 public class ProyectoU1MgApplication implements CommandLineRunner {
 
 	@Autowired
-	private IFachadaCuentaBancaria bancaria;
+	private ProfesorGeneral general;
 
+	@Autowired
+	private ProfesorGeneral general1;
+
+	@Autowired
+	private ProfesorMateria materia;
+
+	@Autowired
+	private ProfesorMateria materia1;
+	
+	@Autowired
+	private IMatriculaService iMatriculaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1MgApplication.class, args);
@@ -28,10 +38,34 @@ public class ProyectoU1MgApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("EJEMPLO SINGLETON");
+		this.general.setNombre("Michael");
+		this.general.setApellido("Garcia");
 
-		BigDecimal interes = this.bancaria.calcularInteres("1213");
-		System.out.println("Interes: " + interes);
+		System.out.println(this.general);
+		System.out.println("-------");
+		System.out.println(this.general1);
+		this.general1.setNombre("Pepito");
+		System.out.println("-------");
+		System.out.println(this.general);
 
+		System.out.println("-------");
+		System.out.println(this.general1);
+		
+		System.out.println("EJEMPLO PROTOTYPE");
+		this.materia.setNombre("Daniel");
+		this.materia.setApellido("Teran");
+		System.out.println(this.materia);
+		System.out.println("-------");
+		System.out.println(this.materia1);
+		
+		Matricula matricula1 = new Matricula();
+		matricula1.setEstudiante(new Estudiante());
+		matricula1.setMateria(new ArrayList<Materia>());
+		matricula1.setNumero("13123");
+		
+		this.iMatriculaService.insertarMatricula(matricula1);
+		
 	}
 
 }
